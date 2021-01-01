0da12bfc7fefbbf1d2abc17f2b5a7a9eed51e523
Sammy Chen <chensongsen03@gmail.com>
2021-01-01 11:42:17 -0600

    Resized calculator

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //basic mathmatic functions
+let cur, ans;
 function add(num1, num2){
     return num1 + num2;
 }
@@ -25,3 +26,23 @@ function operate(operator, num1 , num2){
     }
     return result;
 }
+function populateDisplay(){
+    const buttons = document.querySelectorAll("button");
+    const display = document.querySelector(".display");
+    const operators = "+-/*clear";
+    //iterate through button node list to add its content to the display
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            if(!(button.textContent.includes(operators)) ){ 
+            display.textContent += button.textContent;
+            }
+        });
+    });
+}
+//updates the display after an operator is pressed on
+function update(){
+    const buttons = document.querySelectorAll("button");
+    const display = document.querySelector(".display");
+    display.textContent = ans;
+}
+populateDisplay();
